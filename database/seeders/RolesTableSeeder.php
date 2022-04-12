e8d0e4697b221fda230381d285891a0c722e5d54
Marc Bresson <marc.bresson@etu.imt-nord-europe.fr>
2022-04-12 22:10:50 +0200

    seeder des roles complet

--- database/seeders/RolesTableSeeder.php
+++ database/seeders/RolesTableSeeder.php
@@ -14,39 +14,27 @@ class RolesTableSeeder extends Seeder
      */
     public function run()
     {
-        array(
-            [["gerer_actualite" => 1,"gerer_entite" => 1,"gerer_documentation" => 1,"gerer_evenement" => 1,"gerer_membre" => 1,"gerer_projet" => 1,"gerer_ticket" => 1,],'niveau_admin' => 20,['président·e','vice-président·e',]],
-            [["gerer_actualite" => 1,"gerer_entite" => 1,"gerer_documentation" => 1,"gerer_evenement" => 0,"gerer_membre" => 0,"gerer_projet" => 0,"gerer_ticket" => 1,],'niveau_admin' => 18,['trésorier·e','vice-trésorier·e',]],
-            [["gerer_actualite" => 1,"gerer_entite" => 1,"gerer_documentation" => 1,"gerer_evenement" => 0,"gerer_membre" => 0,"gerer_projet" => 0,"gerer_ticket" => 1,],'niveau_admin' => 17,['secrétaire',]],
-            [["gerer_actualite" => 0,"gerer_entite" => 0,"gerer_documentation" => 0,"gerer_evenement" => 0,"gerer_membre" => 0,"gerer_projet" => 0,"gerer_ticket" => 1,],'niveau_admin' => 13,['responsable communication','responsable logistique','responsable pôle monde', 'responsable pôle Nord', 'responsable pôle MEUD', 'responsable bar', 'responsable spectacle', 'responsable soirée', 'responsable décoration', 'responsable restauration/hébergement', 'responsable sponsorisation', 'responsable animation', 'responsable WEI']],
-            [["gerer_actualite" => 0,"gerer_entite" => 0,"gerer_documentation" => 0,"gerer_evenement" => 0,"gerer_membre" => 0,"gerer_projet" => 0,"gerer_ticket" => 1,],'niveau_admin' => 8,['pôle communication','pôle logistique','pôle monde', 'pôle Nord', 'pôle MEUD', 'pôle bar', 'pôle spectacle', 'pôle soirée', 'pôle décoration', 'pôle restauration/hébergement', 'pôle sponsorisation', 'pôle animation', 'pôle wei',]],
-            [["gerer_actualite" => 0,"gerer_entite" => 0,"gerer_documentation" => 0,"gerer_evenement" => 0,"gerer_membre" => 0,"gerer_projet" => 0,"gerer_ticket" => 0,],'niveau_admin' => 5,['membre',]],
-            [["gerer_actualite" => 0,"gerer_entite" => 0,"gerer_documentation" => 0,"gerer_evenement" => 0,"gerer_membre" => 0,"gerer_projet" => 0,"gerer_ticket" => 0,],'niveau_admin' => 2,['abonné·e',]],
-            [["gerer_actualite" => 0,"gerer_entite" => 0,"gerer_documentation" => 0,"gerer_evenement" => 0,"gerer_membre" => 0,"gerer_projet" => 0,"gerer_ticket" => 0,],'niveau_admin' => 2,['public',]],
+        $roles = array(
+            [["gerer_actualite" => 1,"gerer_entite" => 1,"gerer_documentation" => 1,"gerer_evenement" => 1,"gerer_membre" => 1,"gerer_projet" => 1,"gerer_ticket" => 1,],['niveau_admin' => 20],['président·e','vice-président·e',]],
+            [["gerer_actualite" => 1,"gerer_entite" => 1,"gerer_documentation" => 1,"gerer_evenement" => 0,"gerer_membre" => 0,"gerer_projet" => 0,"gerer_ticket" => 1,],['niveau_admin' => 18],['trésorier·e','vice-trésorier·e',]],
+            [["gerer_actualite" => 1,"gerer_entite" => 1,"gerer_documentation" => 1,"gerer_evenement" => 0,"gerer_membre" => 0,"gerer_projet" => 0,"gerer_ticket" => 1,],['niveau_admin' => 17],['secrétaire',]],
+            [["gerer_actualite" => 0,"gerer_entite" => 0,"gerer_documentation" => 0,"gerer_evenement" => 0,"gerer_membre" => 0,"gerer_projet" => 0,"gerer_ticket" => 1,],['niveau_admin' => 13],['responsable communication','responsable logistique','responsable pôle monde', 'responsable pôle Nord', 'responsable pôle MEUD', 'responsable bar', 'responsable spectacle', 'responsable soirée', 'responsable décoration', 'responsable restauration/hébergement', 'responsable sponsorisation', 'responsable animation', 'responsable WEI']],
+            [["gerer_actualite" => 0,"gerer_entite" => 0,"gerer_documentation" => 0,"gerer_evenement" => 0,"gerer_membre" => 0,"gerer_projet" => 0,"gerer_ticket" => 1,],['niveau_admin' => 8],['pôle communication','pôle logistique','pôle monde', 'pôle Nord', 'pôle MEUD', 'pôle bar', 'pôle spectacle', 'pôle soirée', 'pôle décoration', 'pôle restauration/hébergement', 'pôle sponsorisation', 'pôle animation', 'pôle wei',]],
+            [["gerer_actualite" => 0,"gerer_entite" => 0,"gerer_documentation" => 0,"gerer_evenement" => 0,"gerer_membre" => 0,"gerer_projet" => 0,"gerer_ticket" => 0,],['niveau_admin' => 5],['membre',]],
+            [["gerer_actualite" => 0,"gerer_entite" => 0,"gerer_documentation" => 0,"gerer_evenement" => 0,"gerer_membre" => 0,"gerer_projet" => 0,"gerer_ticket" => 0,],['niveau_admin' => 2],['abonné·e',]],
+            [["gerer_actualite" => 0,"gerer_entite" => 0,"gerer_documentation" => 0,"gerer_evenement" => 0,"gerer_membre" => 0,"gerer_projet" => 0,"gerer_ticket" => 0,],['niveau_admin' => 2],['public',]],
         );
-        DB::table('roles')->insert([
-            [
-                'label' => 'président.e',
-                'niveau_admin' => 7,
-                "gerer_actualite" => 1,
-                "gerer_entite" => 1,
-                "gerer_documentation" => 1,
-                "gerer_evenement" => 1,
-                "gerer_membre" => 1,
-                "gerer_projet" => 1,
-                "gerer_ticket" => 1,
-            ],
-            [
-                'label' => 'public',
-                'niveau_admin' => 0,
-                "gerer_actualite" => 0,
-                "gerer_entite" => 0,
-                "gerer_documentation" => 0,
-                "gerer_evenement" => 0,
-                "gerer_membre" => 0,
-                "gerer_projet" => 0,
-                "gerer_ticket" => 0,
-            ]
-        ]);
+
+        foreach($roles as $role){
+            print_r($role);
+            $labels = $role[2];
+            $niveau_admin = $role[1]['niveau_admin'];
+            $autorisations = $role[0];
+
+            foreach($labels as $label){
+                $tableau = array("label"=>$label, "niveau_admin"=>$niveau_admin);
+                DB::table('roles')->insert(array_merge($tableau, $autorisations));
+            }
+        }
     }
 }

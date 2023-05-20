f76593bab28758509a1e05d3c834ab7f0844ecff
Arthur MATA <arthur.mata@etu.imt-nord-europe.fr>
2023-05-20 10:15:06 +0200

    rajoute seeder entite_sites

--- database/seeders/EntiteSitesSeeder.php
+++ database/seeders/EntiteSitesSeeder.php
@@ -24,6 +24,38 @@ class EntiteSitesSeeder extends Seeder
                 'entite_id' => 1,
                 'site_id' => 2,
             ],
+            [
+                'entite_id' => 14,
+                'site_id' => 2,
+            ],
+            [
+                'entite_id' => 13,
+                'site_id' => 1,
+            ],
+            [
+                'entite_id' => 16,
+                'site_id' => 1,
+            ],
+            [
+                'entite_id' => 8,
+                'site_id' => 1,
+            ],
+            [
+                'entite_id' => 6,
+                'site_id' => 1,
+            ],
+            [
+                'entite_id' => 11,
+                'site_id' => 1,
+            ],
+            [
+                'entite_id' => 2,
+                'site_id' => 1,
+            ],
+            [
+                'entite_id' => 12,
+                'site_id' => 1,
+            ],
         ]);
     }
 }

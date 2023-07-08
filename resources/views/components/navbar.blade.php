dd4b2cbef252769061d4caaead9c21cc0aace005
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 <lucas.naury@sfr.fr>
2023-07-08 17:50:28 +0200

    Add correct links to navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -9,11 +9,12 @@
         
         <ul id="links">
             <x-toggle-theme-btn/>
-            <li class="menu-button"><a href="">Evènements</a></li>
-            <li class="menu-button"><a href="">Associations</a></li>
-            <li class="menu-button"><a href="">About</a></li>
-            <li class="menu-button"><a href="">Contact</a></li>
-            <li class="menu-button connect-button"><a href="">Se connecter</a></li>
+            <li class="menu-button"><a href="/">Accueil</a></li>
+            <li class="menu-button"><a href="/calendrier">Calendrier</a></li>
+            <li class="menu-button"><a href="/entites">Associations</a></li>
+            <li class="menu-button"><a href="/a-propos">About</a></li>
+            {{-- <li class="menu-button"><a href="">Contact</a></li> --}}
+            <li class="menu-button connect-button"><a href="/home">Se connecter</a></li>
         </ul>
 
 

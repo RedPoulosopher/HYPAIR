86f75ac235f74b270815b03511cc53d4e64ac5e7
Lucas Naury <lucas.naury@etu.imt-nord-europe.fr>
2023-07-12 20:31:42 +0200

    Import styles for all default pages

--- resources/views/entite/choix_site.blade.php
+++ resources/views/entite/choix_site.blade.php
@@ -2,34 +2,12 @@
 
 @section('titre', 'Associations')
 
+@pushonce('styles')
+<link rel="stylesheet" href="css/choix_site.css" type="text/css" />
+@endpushonce
+
 @section('content')
 
-<style>
-	.conteneur_boutons {
-		margin-top:40px;
-		display: flex;
-		flex-wrap: wrap;
-		justify-content: center;
-		gap:20px;
-	}
-	.gros_bouton {
-		width:200px;
-		height:200px;
-		border: 1px solid var(--gris_1);
-		border-radius: 15px;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		box-sizing: border-box;
-		padding: 10px;
-		text-align: center;
-		background: var(--gris_3);
-		transition: background 0.15s ease-in-out;
-	}
-	.gros_bouton:hover {
-		background: var(--gris_2);
-	}
-</style>
 
 <div id="wrapper">
 	<!-- Choix du site -->
--- resources/views/entite/mes_entites.blade.php
+++ resources/views/entite/mes_entites.blade.php
@@ -2,6 +2,10 @@
 
 @section('titre', 'Mes entités')
 
+@pushonce('styles')
+<link rel="stylesheet" href="css/mes-entites.css" type="text/css" />
+@endpushonce
+
 @section('content')
 <!-- Contenu principal de la page -->
 <main id="main-content">
@@ -11,15 +15,8 @@
 
         <div class="entites-wrapper">
             
-            <a href="/calendrier" class="service">
-                <img src="/images/logo_services/calendrier.png" alt="logo_calendrier" id="calendrier">
-                <p>Calendrier</p>
-            </a>
-
-            <a href="https://photos.imt-ne.fr" class="service" target="_blank">
-                <img src="/images/logo_services/piwigo.png" alt="logo_piwigo" id="piwigo">
-                <p>Photos</p>
-            </a>
+            <a href="/air">Test entité AIR</a>
+            <a href="/bdh">Test entité BDH</a>
 
         </div>
     </section>

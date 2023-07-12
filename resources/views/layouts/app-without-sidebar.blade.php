ca78d743d8652dde33795194a813baa2e44a5caa
Lucas Naury <lucas.naury@etu.imt-nord-europe.fr>
2023-07-12 20:56:58 +0200

    Move general + variables + scrollbar to default import in layouts

--- resources/views/layouts/app-without-sidebar.blade.php
+++ resources/views/layouts/app-without-sidebar.blade.php
@@ -6,6 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <title>@yield('title', 'Site Web') - HypAIR</title>
         
+        <link rel="stylesheet" href="css/default.css" type="text/css" />
         @stack('styles')
     </head>
     
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <title>@yield('titre', 'Site Web') - HypAIR</title>
         
+        <link rel="stylesheet" href="css/default.css" type="text/css" />
         @stack('styles')
     </head>
     

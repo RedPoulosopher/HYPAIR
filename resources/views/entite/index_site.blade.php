21f7d4904045cddb2b3ab3c3a48d8255c4e3531a
Arthur MATA <arthur.mata@etu.imt-nord-europe.fr>
2023-07-17 22:55:27 +0200

    entite component with link in parameter

--- resources/views/entite/index_site.blade.php
+++ resources/views/entite/index_site.blade.php
@@ -8,12 +8,12 @@
         @foreach ($bureaux as $bureau)
             <h1>Entités du {{ $bureau->nom }}</h1>
             <div class="liste_comite_club">
-                <x-entite :asso="$bureau" />
+                <x-entite :asso="$bureau" :destination="$bureau->lien_relatif()" />
             </div>
 
             <div class="liste_comite_club">
                 @foreach ($comites_clubs_dependants[$bureau->ratachement->value] as $comite_club)
-                    <x-entite :asso="$comite_club" />
+                    <x-entite :asso="$comite_club" :destination="$comite_club->lien_relatif()" />
                 @endforeach
             </div>
         @endforeach
@@ -22,7 +22,7 @@
             <h1>Entites indépendantes</h1>
             <div class="liste_comite_club">
                 @foreach ($entites_independantes as $entite_independante)
-                    <x-entite :asso="$entite_independante" />
+                    <x-entite :asso="$entite_independante" :destination="$entite_independante->lien_relatif()" />
                 @endforeach
             </div>
         @endif

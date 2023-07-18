76fc8f96da5cd71b47cfe2073bf45c8b53a924bb
Arthur MATA <arthur.mata@etu.imt-nord-europe.fr>
2023-07-18 20:14:48 +0200

    fix title in header

--- resources/views/layouts/app-without-sidebar.blade.php
+++ resources/views/layouts/app-without-sidebar.blade.php
@@ -1,48 +1,49 @@
 <!DOCTYPE html>
 <html lang="fr">
-    <head>
-        <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <title>@yield('title', 'Site Web') - HypAIR</title>
-        
-        <link rel="stylesheet" href="css/default.css" type="text/css" />
-        @stack('styles')
-    </head>
-    
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>@yield('titre', 'Site Web') - HypAIR</title>
+
+    <link rel="stylesheet" href="css/default.css" type="text/css" />
+    @stack('styles')
+</head>
+
 @include('layouts.theme')
 
-    <body>
+<body>
 
-        @php
-            // Code pour gérer le login utilisateur
-            use App\Services\GestionPhotoDeProfil;
-            if (Auth::check()) {
-                $user = Auth::user();
-                $user["chemin_photo_de_profil"] = GestionPhotoDeProfil::chemin_utilisateur_photo($user);
-            }
-        @endphp
-        
-        <!-- Barre de navigation -->
-        <!-- Si l'utilisateur est connecté : faire apparaître sa PFP au lieu du bouton Se Connecter -->
-        @if (Auth::check())
-            <x-navbar :isConnected="true" :user="$user" />
+    @php
+        // Code pour gérer le login utilisateur
+        use App\Services\GestionPhotoDeProfil;
+        if (Auth::check()) {
+            $user = Auth::user();
+            $user['chemin_photo_de_profil'] = GestionPhotoDeProfil::chemin_utilisateur_photo($user);
+        }
+    @endphp
 
-        <!-- Sinon : mettre le bouton Se Connecter (la navbar normale) -->   
-        @else
-            <x-navbar :isConnected="false" :user="[]" />
-        @endif
+    <!-- Barre de navigation -->
+    <!-- Si l'utilisateur est connecté : faire apparaître sa PFP au lieu du bouton Se Connecter -->
+    @if (Auth::check())
+        <x-navbar :isConnected="true" :user="$user" />
 
-        <!-- Contenu de la page -->
-        <div id="content">
-            
-            @yield('content')
+        <!-- Sinon : mettre le bouton Se Connecter (la navbar normale) -->
+    @else
+        <x-navbar :isConnected="false" :user="[]" />
+    @endif
 
-            <footer>
-                Fait avec amour par l’AIR - Tous droits réservés
-            </footer>
-        </div>
-        
-    </body>
-    
+    <!-- Contenu de la page -->
+    <div id="content">
+
+        @yield('content')
+
+        <footer>
+            Fait avec amour par l’AIR - Tous droits réservés
+        </footer>
+    </div>
+
+</body>
+
 </html>

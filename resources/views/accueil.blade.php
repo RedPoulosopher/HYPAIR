d73c4c9199f01bd74969bae10991f18481e04706
Arthur MATA <arthur.mata@etu.imt-nord-europe.fr>
2023-07-26 22:05:12 +0200

    service component

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -16,29 +16,12 @@
 
             <div class="services-wrapper">
 
-                <a href="/calendrier" class="service">
-                    <img src="/images/logo_services/calendrier.png" alt="logo_calendrier" id="service-calendrier">
-                    <p>Calendrier</p>
-                </a>
-
-                <a href="https://photos.imt-ne.fr" class="service" target="_blank">
-                    <img src="/images/logo_services/piwigo.png" alt="logo_piwigo" id="service-piwigo">
-                    <p>Photos</p>
-                </a>
-
-                <a href="https://peertube.imt-ne.fr" class="service" target="_blank">
-                    <img src="/images/logo_services/peertube.png" alt="logo_peertube" id="service-peertube">
-                    <p>Vidéos</p>
-                </a>
-
-                <a href="https://gitlab.etu.imt-nord-europe.fr" class="service" target="_blank">
-                    <img src="/images/logo_services/gitlab.png" alt="logo_gitlab" id="service-gitlab">
-                    <p>GitLab</p>
-                </a>
+                <x-service nom="Piwigo" destination='https://photos.imt-ne.fr' color=#FF7800 logo='/images/piwigo.png'></x-service>
+                <x-service nom="PeerTube" destination='https://peertube.imt-ne.fr' color=#727272 logo='/images/peertube.png'></x-service>
+                <x-service nom="GitLab" destination='https://gitlab.etu.imt-nord-europe.fr' color=#E24329 logo='/images/gitlab.png'></x-service>
 
             </div>
         </section>
-
 
         <section>
             <h1>Actualités</h1>
---
+++ resources/views/components/service.blade.php
@@ -0,0 +1,9 @@
+<a class="entite" href="{{ $destination }}">
+    <div class="logo ombre_petite">
+        <div class="cercle" style="border-color: {{ $color }}"></div>
+        <img src="{{ $logo }}" />
+    </div>
+    <div class="info" style="text-align:center;">
+        <p class="nom">{{ $nom }}</p>
+    </div>
+</a>

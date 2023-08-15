c23b2737aac21a95993e0aa4c366bc0b4e4cfeba
Lucas <lucas.naury@etu.imt-nord-europe.fr>
2023-08-15 16:19:33 +0200

    Improve post component responsiveness

--- resources/views/components/post.blade.php
+++ resources/views/components/post.blade.php
@@ -13,27 +13,16 @@ use App\Http\Controllers\PostController;
         <div class="details">
             <h2>{{ $post->titre }}</h2>
             <p>Posté par {{ $post->entite->nom }}<span class="separator">•</span>Il y a {{ PostController::date_apparition_to_duration($post->date_apparition)}}</p>
-
-            <div class="tags">
-                <div class="tag" style="background-color: {{ PostController::stringToColorCode('IMPORTANT') }};">
-                    <p>IMPORTANT</p>
-                </div>
-                <div class="tag" style="background-color: {{ PostController::stringToColorCode('BDH') }};">
-                    <p>BDH</p>
-                </div>
-                <div class="tag" style="background-color: {{ PostController::stringToColorCode('Gala') }};">
-                    <p>Gala</p>
-                </div>
-                <div class="tag" style="background-color: {{ PostController::stringToColorCode('Soirée') }};">
-                    <p>Soirée</p>
-                </div>
-                <div class="tag" style="background-color: {{ PostController::stringToColorCode('test') }};">
-                    <p>test</p>
-                </div>
-            </div>
-
         </div>
-
+        
+        <div class="tags">
+            <div class="tag" style="background-color: {{ PostController::stringToColorCode('IMPORTANT') }};">IMPORTANT</div>
+            <div class="tag" style="background-color: {{ PostController::stringToColorCode('BDH') }};">BDH</div>
+            <div class="tag" style="background-color: {{ PostController::stringToColorCode('Gala') }};">Gala</div>
+            <div class="tag" style="background-color: {{ PostController::stringToColorCode('Soirée') }};">Soirée</div>
+            <div class="tag" style="background-color: {{ PostController::stringToColorCode('test') }};">test</div>
+        </div>
+        
         <div class="arrow-display">
             {{-- Flèche rouge pour dérouler la description --}}
             <svg class="arrow" id="arrow-{{$post->id}}" width="42" height="24" viewBox="0 0 42 24"

ecb91a601a638af63ec4c2ebed868e956574e3c6
Lucas <lucas.naury@etu.imt-nord-europe.fr>
2023-08-22 19:02:00 +0200

    A propos style

--- resources/views/a_propos.blade.php
+++ resources/views/a_propos.blade.php
@@ -10,15 +10,15 @@
 
     <main id="main-content">
         <section>
-            <h1>À propos de l'application HypAIR</h1>
-            <article class="card">
+            <h1>À propos de HypAIR</h1>
+            <article>
                 <p>HypAIR est un site qui se veut être un complément à la vie associative en
                     ajoutant des fonctionnalités et des services qui amélioreront la vie étudiante (calendrier,
-                    évènements, posts...) et centraliseront les infromations des comités et des associations de l'écoles.
+                    évènements, posts...) et centraliseront les infromations des comités et des associations de l'école.
                     Nous voulons être présents à
                     Lille comme à Douai, et à l'avenir, on l'espère, sur tous les campus !</p>
-                <p>Le développement d'HypAIR a débuté il y a 3 ans, le site est encore loin d'être fini. Il
-                    y a encore beaucoup d'améliorations à apporter. Donc n'hésitez
+                <p>Le développement d'HypAIR a débuté il y a 3 ans, mais il reste
+                     encore beaucoup d'améliorations à apporter. Donc n'hésitez
                     pas à nous faire des retours sur votre ressenti et sur les nouveautés que vous aimeriez voir !</p>
                 <p>D'ailleurs si quelqu'un est chaud en dessin, on cherche désespérement un vrai logo pour le site. Si une
                     bienveillante personne propose un magnifique logo pour HypAIR, il aura son nom plaqué or pendant une

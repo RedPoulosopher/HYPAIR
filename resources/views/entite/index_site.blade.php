5a6426694ca4bdd24e3da00134463f5a8d043d19
Arthur MATA <arthur.mata@etu.imt-nord-europe.fr>
2023-08-23 19:19:04 +0200

    can hide entites

--- resources/views/entite/index_site.blade.php
+++ resources/views/entite/index_site.blade.php
@@ -9,23 +9,29 @@
         <x-switch-campus :campus="$site" />
         <section>
             @foreach ($bureaux as $bureau)
-                <h1>Entités du {{ $bureau->nom }}</h1>
-                <div class="liste_comite_club">
-                    <x-entite :asso="$bureau" :destination="$bureau->lien_relatif()" />
-                </div>
+                @if (!$bureau->hidden)
+                    <h1>Entités du {{ $bureau->nom }}</h1>
+                    <div class="liste_comite_club">
+                        <x-entite :asso="$bureau" :destination="$bureau->lien_relatif()" />
+                    </div>
 
-                <div class="liste_comite_club">
-                    @foreach ($comites_clubs_dependants[$bureau->ratachement->value] as $comite_club)
-                        <x-entite :asso="$comite_club" :destination="$comite_club->lien_relatif()" />
-                    @endforeach
-                </div>
+                    <div class="liste_comite_club">
+                        @foreach ($comites_clubs_dependants[$bureau->ratachement->value] as $comite_club)
+                            @if (!$comite_club->hidden)
+                                <x-entite :asso="$comite_club" :destination="$comite_club->lien_relatif()" />
+                            @endif
+                        @endforeach
+                    </div>
+                @endif
             @endforeach
 
             @if (count($entites_independantes ?? []) > 0)
                 <h1>Entites indépendantes</h1>
                 <div class="liste_comite_club">
                     @foreach ($entites_independantes as $entite_independante)
-                        <x-entite :asso="$entite_independante" :destination="$entite_independante->lien_relatif()" />
+                        @if (!$entite_independante->hidden)
+                            <x-entite :asso="$entite_independante" :destination="$entite_independante->lien_relatif()" />
+                        @endif
                     @endforeach
                 </div>
             @endif

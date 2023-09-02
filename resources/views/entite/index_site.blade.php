f047a7c57b10ea56768fe6bdf55919e8c78ba38f
Arthur MATA <arthur.mata@etu.imt-nord-europe.fr>
2023-09-02 20:58:19 +0200

    open dunkerque, alençon, valenciennes assos

--- resources/views/entite/index_site.blade.php
+++ resources/views/entite/index_site.blade.php
@@ -8,32 +8,36 @@
     <main id="main-content">
         <x-switch-campus :campus="$site" />
         <section>
-            @foreach ($bureaux as $bureau)
-                @if (!$bureau->hidden)
-                    <h1>Entités du {{ $bureau->nom }}</h1>
-                    <div class="liste_comite_club">
-                        <x-entite :asso="$bureau" :destination="$bureau->lien_relatif()" />
-                    </div>
+            @if (count($bureaux) > 0)
+                @foreach ($bureaux as $bureau)
+                    @if (!$bureau->hidden)
+                        <h1>Entités du {{ $bureau->nom }}</h1>
+                        <div class="liste_comite_club">
+                            <x-entite :asso="$bureau" :destination="$bureau->lien_relatif()" />
+                        </div>
 
+                        <div class="liste_comite_club">
+                            @foreach ($comites_clubs_dependants[$bureau->ratachement->value] as $comite_club)
+                                @if (!$comite_club->hidden)
+                                    <x-entite :asso="$comite_club" :destination="$comite_club->lien_relatif()" />
+                                @endif
+                            @endforeach
+                        </div>
+                    @endif
+                @endforeach
+
+                @if (count($entites_independantes ?? []) > 0)
+                    <h1>Entites indépendantes</h1>
                     <div class="liste_comite_club">
-                        @foreach ($comites_clubs_dependants[$bureau->ratachement->value] as $comite_club)
-                            @if (!$comite_club->hidden)
-                                <x-entite :asso="$comite_club" :destination="$comite_club->lien_relatif()" />
+                        @foreach ($entites_independantes as $entite_independante)
+                            @if (!$entite_independante->hidden)
+                                <x-entite :asso="$entite_independante" :destination="$entite_independante->lien_relatif()" />
                             @endif
                         @endforeach
                     </div>
                 @endif
-            @endforeach
-
-            @if (count($entites_independantes ?? []) > 0)
-                <h1>Entites indépendantes</h1>
-                <div class="liste_comite_club">
-                    @foreach ($entites_independantes as $entite_independante)
-                        @if (!$entite_independante->hidden)
-                            <x-entite :asso="$entite_independante" :destination="$entite_independante->lien_relatif()" />
-                        @endif
-                    @endforeach
-                </div>
+            @else
+                <p class="should-be-connected no-content">Pas d'associations pour le moment.</p>
             @endif
         </section>
     </main>

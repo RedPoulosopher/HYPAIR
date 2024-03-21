cc5e46020a5b610b17348ba9042d411a97d7b4c8
Lucas NAURY <lucas.naury@etu.imt-nord-europe.fr>
2024-03-21 15:09:33 +0100

    Change page names

--- resources/views/layouts/erreur.blade.php
+++ resources/views/layouts/erreur.blade.php
@@ -4,7 +4,7 @@
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta http-equiv="X-UA-Compatible" content="ie=edge">
-		<title>Erreur @yield('code_erreur') - HypAIR</title>
+		<title>HypAIR - Erreur @yield('code_erreur')</title>
 		
 		@include('pwa.meta')
 

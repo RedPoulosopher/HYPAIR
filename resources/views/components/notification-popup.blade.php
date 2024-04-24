d74c4dbf7c6661da5459b4b0edbba52936581dc5
Lucas NAURY <lucas.naury@etu.imt-nord-europe.fr>
2024-04-24 20:17:58 +0200

    Notification message when no support

--- resources/views/components/notification-popup.blade.php
+++ resources/views/components/notification-popup.blade.php
@@ -1,5 +1,5 @@
-<div class="select-popup" class="popup">
-    <div class="popup-content" class="card">
+<div id="select-popup" class="popup">
+    <div class="popup-content card" id="choix">
 
         <h2>Autoriser les notifications ?</h2>
         <div id="liste-choix">
@@ -8,17 +8,33 @@
         </div>
     
     </div>
+
+    <div class="popup-content card hidden" id="info">
+        <h2>Votre navigateur ne supporte pas les notifications :(</h2>
+        <p>Essayez en un autre (Chrome par exemple) si vous souhaitez les recevoir</p>
+        
+        <div id="liste-choix">
+            <a href="" onclick="choixNotifs(event, false)">OK</a>
+        </div>
+    </div>
 </div>
 
 <script>
 
 var popup = document.getElementById("select-popup")
+var listeChoix = document.getElementById("choix")
+var unsupportedText = document.getElementById("info")
 
-// If first time seing popup and browser supports notifications, show popup
-var supportsNotifications = 'Notification' in window
+// If first time seing popup, show popup
 var popupAlreadySeen = localStorage.getItem("notifications-authorized") != null
-if(supportsNotifications && !popupAlreadySeen){
+if(!popupAlreadySeen){
     popup.classList.add("visible")
+}
+// If browser doesn't support notifications, change popup text to tell user to change browser
+var supportsNotifications = 'Notification' in window
+if(!supportsNotifications && !popupAlreadySeen){
+    unsupportedText.classList.remove("hidden")
+    listeChoix.classList.add("hidden")
 }
 
 

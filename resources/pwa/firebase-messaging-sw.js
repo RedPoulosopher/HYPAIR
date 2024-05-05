bcaba5300ef8c0b476830b6de4c10825eef296bb
Lucas NAURY <lucas.naury@etu.imt-nord-europe.fr>
2024-05-05 19:25:19 +0200

    Add badge to icon

--- resources/pwa/firebase-messaging-sw.js
+++ resources/pwa/firebase-messaging-sw.js
@@ -10,6 +10,7 @@ self.addEventListener('push', function (event) {
     event.waitUntil(self.registration.showNotification(notifContent.title, {
         body: notifContent.body,
         icon: "images/logo_air.png",
+        badge: "images/logo_air.png",
         data: {
             notifUrl: notifData.url // url quand on clique
         }

f3a7b827068e6e768994f740ac761b3cc39d9a57
Lucas NAURY <lucas.naury@etu.imt-nord-europe.fr>
2024-05-13 23:05:24 +0200

    Add event.waitUntil to prevent warning logs

--- resources/pwa/firebase-messaging-sw.js
+++ resources/pwa/firebase-messaging-sw.js
@@ -3,13 +3,18 @@ self.addEventListener("install", event => {
 });
 
 // Notifications
-self.addEventListener('push', async (event) => {
+self.addEventListener('push', (event) => {
     const payload = event.data.json()
-    
-    const notifContent = payload.notification
-    const notifData = payload.data
 
     console.log(payload)
+
+    return event.waitUntil(handlePushNotification(payload));
+})
+
+const handlePushNotification = async (payload) => {
+    // Extract notification data
+    const notifContent = payload.notification
+    const notifData = payload.data
 
     // Create notification objects
     let notificationTitle = notifContent.title || "Nouvelle notification";
@@ -22,50 +27,54 @@ self.addEventListener('push', async (event) => {
         }
     };
 
-    if(notifData.tag == "POST"){
-        // On ajoute à la notifications les infos de tag
-        notificationOptions.tag = notifData.tag //Permet d'écraser les précédentes notifications ayant le même tag sans renotifier l'utilisateur
-    
-        // On stocke des données dans la notification client afin de pouvoir les récupérer par exemple si on veut fusionner 2 notifications par la suite
-        notificationOptions.data.NB_POSTS = 1                 // Le nombre de posts s'il y en a plusieurs
-        notificationOptions.data.ENTITES = [notifData.entite] // La liste des entités qui ont posté
+    //Envoi de la notif de base si ce n'est pas un post
+    if(notifData.tag != "POST"){
+        return registration.showNotification(notificationTitle, notificationOptions);
+    }
 
 
+    // -------------------------- NOTIFICATION TAG -------------------------- //
+    // L'ajout du tag permet d'écraser les précédentes notifications ayant le même tag
+    // sans renotifier l'utilisateur
+    notificationOptions.tag = notifData.tag 
+
+    // On stocke des données dans la notification client afin de pouvoir les récupérer par exemple si on veut fusionner 2 notifications par la suite
+    notificationOptions.data.NB_POSTS = 1                 // Le nombre de posts s'il y en a plusieurs
+    notificationOptions.data.ENTITES = [notifData.entite] // La liste des entités qui ont posté
 
 
-        // On regarde s'il y a déjà une notification avec ce tag
-        const currentNotification = await getCurrentNotifWithTag(notifData.tag) //On vérifie si une notification n'est pas déjà présente pour les fusionner
-        // S'il y a déjà une notification avec le tag "POST", on les fusionne
-        if(currentNotification){
-            // On indique qu'il y a un post supplémentaire
-            notificationOptions.data.NB_POSTS = parseInt(currentNotification.data.NB_POSTS) + 1
+    // On regarde s'il y a déjà une notification avec ce tag
+    const currentNotification = await getCurrentNotifWithTag(notifData.tag) //On vérifie si une notification n'est pas déjà présente pour les fusionner
+    // S'il y a déjà une notification avec le tag "POST", on les fusionne
+    if(currentNotification){
+        // On indique qu'il y a un post supplémentaire
+        notificationOptions.data.NB_POSTS = parseInt(currentNotification.data.NB_POSTS) + 1
 
-            // On stocke la liste des entités qui ont posté (sans duplicata)
-            notificationOptions.data.ENTITES = [...new Set(currentNotification.data.ENTITES.concat(notificationOptions.data.ENTITES))];
-    
-            // On change le lien pour rediriger vers la page d'accueil
-            notificationOptions.data.notifUrl = "/"
-            // Quand il y a plusieurs posts d'entités différentes, l'image est le logo de l'air
-            if(notificationOptions.data.ENTITES.length > 1){
-                notificationOptions.icon = "images/notifications/logo_air_carre.png"
-            }
-            
-            // On change le titre
-            notificationTitle = notificationOptions.data.NB_POSTS + " nouveaux posts"
-            // On change le texte
-            if(notificationOptions.data.ENTITES.length > 3){
-                entityList = notificationOptions.data.ENTITES.filter((element,index) => index < 3);
-                notificationOptions.body = "De " + entityList.join(", ") + "..."
-            }else{
-                notificationOptions.body = "De " + notificationOptions.data.ENTITES.join(", ")
-            }
+        // On stocke la liste des entités qui ont posté (sans duplicata)
+        notificationOptions.data.ENTITES = [...new Set(currentNotification.data.ENTITES.concat(notificationOptions.data.ENTITES))];
+
+        // On change le lien pour rediriger vers la page d'accueil
+        notificationOptions.data.notifUrl = "/"
+        // Quand il y a plusieurs posts d'entités différentes, l'image est le logo de l'air
+        if(notificationOptions.data.ENTITES.length > 1){
+            notificationOptions.icon = "images/notifications/logo_air_carre.png"
+        }
+        
+        // On change le titre
+        notificationTitle = notificationOptions.data.NB_POSTS + " nouveaux posts"
+        // On change le texte
+        if(notificationOptions.data.ENTITES.length > 3){
+            entityList = notificationOptions.data.ENTITES.filter((element,index) => index < 3);
+            notificationOptions.body = "De " + entityList.join(", ") + "..."
+        }else{
+            notificationOptions.body = "De " + notificationOptions.data.ENTITES.join(", ")
         }
     }
 
+    
     //Envoi de la notif fusionnée
-    return registration.showNotification(notificationTitle,notificationOptions);
-
-})
+    return registration.showNotification(notificationTitle, notificationOptions)
+}
 
 const getCurrentNotifWithTag = async (tag)=>{
     const notifications = await registration.getNotifications()

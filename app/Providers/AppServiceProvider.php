f49a09d0a60b6930a2852c5fe4d7d3afbcf4e979
Mathilde Pascal <mathilde.pascal@etu.imt-nord-europe.fr>
2024-09-19 14:07:47 +0200

    modif vite build

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 
@@ -24,5 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Vite::useBuildDirectory("");
+        Vite::useManifestFilename("vite-manifest.json");
     }
 }
--- vite.config.js
+++ vite.config.js
@@ -1,114 +1,60 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import fs from 'fs';
 import { resolve } from 'path'
 import { rm } from 'node:fs/promises'
 
-let files = [];
-let getFilesInSubdirectories = function (dir) {
-    fs.readdirSync(dir).forEach(filename => {
-        const absolute = `${dir}/${filename}`;
-        if (fs.statSync(absolute).isDirectory()) return getFilesInSubdirectories(absolute);
-        else if (!filename.startsWith('_')) return files.push(absolute);
-    });
-}
-
-// -------------------- Fonts -------------------- //
-getFilesInSubdirectories('resources/fonts/');
-files = files.filter(path => /\.(ttf)$/.test(path))
-
-// --------------------- CSS --------------------- //
-getFilesInSubdirectories('resources/css/');
-
-// --------------------- JS ---------------------- //
-getFilesInSubdirectories('resources/js');
-// PWA
-files.push("resources/pwa/sw.js")
-files.push("resources/pwa/firebase-messaging-sw.js")
-// Notifications
-files.push("resources/notifications/fcm.js")
-
-// -------------------- Images ------------------- //
-getFilesInSubdirectories('resources/images/');
-
-
-var excludeFromVersioning = ['sw.js', 'firebase-messaging-sw.js', 'default.css', 'offline.css']
-var changePath = [
-    //SW
-    {input : 'sw.js', output: '/sw.js'},
-    {input : 'firebase-messaging-sw.js', output: '/firebase-messaging-sw.js'},
-    
-    // Base CSS
-    {input : 'default.css', output: '/assets/css/default.css'},
-    {input : 'offline.css', output: '/assets/css/offline.css'},
-]
-
-function generateOutputPath(chunkInfo, ext){
-    // Setup the default build path
-    var path = 'assets/[name]-[hash].' + ext
-
-    var filename = chunkInfo.facadeModuleId != undefined ?
-                        chunkInfo.facadeModuleId.split('\\').pop()
-                        : `${chunkInfo.name}.${ext}`.replace('.[ext]','')
-
-    // Remove hash
-    for(var i=0; i<excludeFromVersioning.length; i++){
-        var str = excludeFromVersioning[i];
-        
-        if(filename == str){
-            //Remove hash
-            path = path.replace('-[hash]', '')
-            break
-        }
+function assetName(assetInfo, ext)  {
+    console.log(assetInfo)
+    if(rootFiles.includes(assetInfo.name)) {
+        return '[name]' + ext;
     }
-
-    // Move to another folder than assets
-    for(var i=0; i<changePath.length; i++){
-        var str = changePath[i].input;
-
-        
-        if(filename == str){
-            var newPrefix = changePath[i].output.replace(str,'').substring(1);
-
-            //Replace assets prefix with new one
-            path = path.replace('assets/', newPrefix)
-            break
-        }
-    }
-
-    return path
+    return 'assets/[name]' + ext;
 }
 
 // -------------------------------- VITE CONFIG -------------------------------- //
 
-export default defineConfig({
-        base: '/',
-        plugins: [
-            laravel({
-                buildDirectory: '.',
-                input: files,
-                refresh: true,
-            }),
+const rootFiles = [
+    'sw',
+    'firebase-messaging-sw'
+]
 
-            // Plugin to clear the assets folder on build
-            // (instead of the whole public folder when emptyOutDir is true)
-            {
-                name: "Cleaning assets folder",
-                async buildStart() {
-                  await rm(resolve(__dirname, 'public/assets'), { recursive: true, force: true });
-                }
+export default defineConfig({
+    base: '/',
+    plugins: [
+        laravel({
+            buildDirectory: '.',
+            input: [
+                'resources/js/pwa/sw.js',
+                'resources/js/notifications/fcm.js',
+                'resources/js/pwa/firebase-messaging-sw.js',
+                'resources/css/default.scss',
+                'resources/css/offline.scss',
+            ],
+            refresh: true,
+        }),
+        {
+            name: "Cleaning assets folder",
+            async buildStart() {
+                await rm(resolve(__dirname, 'public/assets'), { recursive: true, force: true });
             }
-        ],
-        build: {        
-            manifest: 'vite-manifest.json',
-            emptyOutDir: false,
-            rollupOptions: {
-                output: {
-                    // Change file output
-                    entryFileNames: ((chunkInfo) => generateOutputPath(chunkInfo, 'js')),
-                    chunkFileNames: ((chunkInfo) => generateOutputPath(chunkInfo, 'js')),
-                    assetFileNames: ((chunkInfo) => generateOutputPath(chunkInfo, '[ext]'))
-                }
-            }
+        }
+
+    ],
+    resolve: {
+        alias: {
+            '@': '/resources/js',
+            '$': '/resources'
         },
+    },
+    build: {
+        manifest: 'vite-manifest.json',
+        emptyOutDir: false,
+        rollupOptions: {
+            output: {
+                entryFileNames: (assetInfo) => assetName(assetInfo, '.js'),
+                chunkFileNames: (assetInfo) => assetName(assetInfo, '.js'),
+                assetFileNames: (assetInfo) => assetName(assetInfo, '[extname]')
+            }
+        }
+    }
 });

ac437c95debcdd457c5a50bcd4e5b7f579a71731
gaspard.beunier <77894810+1m0ut0n@users.noreply.github.com>
2025-01-07 21:55:19 +0100

    avec le bouton dans la navbar c'est mieux

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -20,7 +20,7 @@
             <li class="menu-button {{ request()->is('entites*') ? 'active' : '' }}"><a href="/entites">Associations</a></li>
 
             {{-- A afficher que pendant les campagnes --}}
-            {{-- <li class="menu-button {{ request()->is('campagnes') ? 'active' : '' }}"><a href="/campagnes">Campagnes</a></li> --}}
+            <li class="menu-button {{ request()->is('campagnes') ? 'active' : '' }}"><a href="/campagnes">Campagnes</a></li>
 
 
             <li class="menu-button {{ request()->is('contact') ? 'active' : '' }}"><a href="/contact">Contact</a></li>
